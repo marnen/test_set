501101822635b75c0f36435605f149b1ceede51b
Marnen Laibow-Koser <marnen@marnen.org>
2009-08-07 01:32:27 -0400

    Get median working.

--- test_set.rb
+++ test_set.rb
@@ -8,4 +8,16 @@ class TestSet
   def average
     @array.inject(0.0) {|total, n| total + n} / @array.size
   end
+  
+  # Returns the median of all the elements in the set (that is, the middle value
+  # if there are an odd number of elements, or the average of the two middle values if even)
+  def median
+    sorted = @array.sort
+    middle = ((@array.size + 1.0) / 2) - 1 # subscript of middle element of array, with .5 if there are two middle values
+    if middle.floor == middle # middle is an integer
+      sorted[middle]
+    else
+      (sorted[middle.floor] + sorted[middle.ceil]) / 2.0
+    end
+  end
 end
