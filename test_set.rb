dd72b21ae13455ef80e4094eebd55284d8ddd647
Marnen Laibow-Koser <marnen@marnen.org>
2009-08-07 01:22:22 -0400

    Define average method.

--- test_set.rb
+++ test_set.rb
@@ -1,2 +1,11 @@
 class TestSet
+  # Constructs a new TestSet object with any number of arguments.
+  def initialize(*args)
+    @array = args
+  end
+  
+  # Returns the average of all the elements in the set.
+  def average
+    @array.inject(0.0) {|total, n| total + n} / @array.size
+  end
 end
